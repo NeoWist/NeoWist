70d7911983cd32f5c42f1c4eae9f95f452d2aba7
NeoWist <93931544+NeoWist@users.noreply.github.com>
2023-11-18 17:22:24 +0530

    Create Amal_Resume.py
    trying out resume,docx maker python code

---
+++ Amal_Resume.py
@@ -0,0 +1,93 @@
+**Amal K P**
+
+**Contact:**
+- Address: Kokkandathil House, Kuttichira - 680724, Chalakudy, Kerala, India
+- Email: amalkp814@gmail.com
+- Phone: +91 8304024201
+- LinkedIn: [LinkedIn Profile](https://www.linkedin.com/in/amalkp814/)
+- GitHub: [GitHub Profile](https://github.com/NeoWist)
+
+---
+
+**Objective:**
+Motivated AI/ML Engineer with a strong academic background and hands-on experience in machine learning, data analysis, and software development. Eager to contribute innovative solutions to real-world problems and thrive in a collaborative and growth-focused environment.
+
+---
+
+**Education:**
+
+**1. Master's Certification Programs:**
+- Simplilearn Academy
+  - Artificial Intelligence Engineer, Dec 2021 - Dec 2023
+  - Enrolled in the Artificial Intelligence Engineer program, focusing on data analysis, Python programming, and machine learning.
+  - Studying computer vision techniques, natural language processing, and mathematics and statistics for data science.
+
+**2. M.Tech:**
+- College of Engineering, Thiruvananthapuram
+  - Master of Technology (M.Tech), Applied Electronics and Instrumentation Engineering, 2017 - 2020
+  - CGPA: 7.02
+  - Activities and Achievements: [Details as provided]
+
+**3. B.Tech:**
+- College of Engineering Chengannur
+  - Bachelor of Technology (B.Tech), Electronics And Instrumentation Engineering, 2012 - 2016
+  - CGPA: 7.19
+  - Activities and Achievements: [Details as provided]
+
+---
+
+**Skills:**
+- Python Programming
+- Systems Design
+- Troubleshooting
+- Project Management
+- Data Analysis and Interpretation
+- Machine Learning Algorithms
+- Deep Learning with Keras and TensorFlow
+- Computer Vision Techniques
+- Natural Language Processing (NLP)
+- IoT (Internet of Things)
+- C
+- C++
+- MikroC
+- Arduino
+- Microsoft Office
+- Matlab
+- LabVIEW
+- Electronics and Instrumentation Engineering
+- Industrial Automation
+- SCADA (Supervisory Control and Data Acquisition)
+- HMI (Human-Machine Interface)
+- Proteus
+- Diptrace
+- PLC Programming
+- Linux (Ubuntu, Raspbian, etc.)
+- Windows
+
+---
+
+**Professional Experience:**
+
+**1. Data Science & Machine Learning Internship**
+- Gilbert Research Center, Jul 2023 - Sep 2023 (3 months)
+  - Completed a 2-month Data Science & Machine Learning Internship, gaining comprehensive knowledge in mathematics, Python programming, statistics, and machine learning.
+  - Explored advanced topics including neural networks, deep learning, and image processing, particularly in the context of medical imaging.
+  - Received a Verified Certificate and a Letter of Recommendation from Gilbert Research Center.
+
+**2. Electronics and Instrumentation Engineer**
+- K J Bee Farm, Jul 2019 - Jul 2023 (4 years 1 month)
+  - Acquired expertise in designing systems to reduce honey moisture and ensure food safety using temperature and humidity monitoring and controlling, by making use of different sensors and control units.
+  - Set up an air filtering system for the honey packing unit.
+  - Led the development of honey extraction machines and related tools.
+  - Demonstrated strong troubleshooting and problem-solving skills.
+
+---
+
+**Certifications and Achievements:**
+- Featured in local news for building a sanitization tunnel during the COVID crisis with two friends.
+- Featured in two different newspapers during B.Tech:
+  - Developed an automatic waste bin called "Green Bin" with a group of colleagues, associated with Thiruvananthapuram Railway Division, promoting cleanliness.
+
+---
+
+This text code can be easily copied and pasted into various platforms or text editors. Let me know if you need any further adjustments!
